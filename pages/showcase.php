92d3ec965370b274e5483ff2614272e65ec9df80
mamstha <86461385+mamstha@users.noreply.github.com>
2021-12-15 11:52:00 +0545

    metro-coffee

--- pages/showcase.php
+++ pages/showcase.php
@@ -2,9 +2,74 @@
         <div id="myCarosel" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <div class="carousel-caption d-none d-md-block">
-                      <div class="container">
+                    <img src="img/background-coffee.png" class="d-block w-100" alt="First Slide">
+                        <a href="#" class="carousel-control-next" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                        <a href="#" class="carousel-control-prev" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+
+                        <a href="#" class="carousel-control-next" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
+                    
+                    <div class="carousel-caption">
+                        <h3>
+                            Serving You Ethically Sourced, Freshly Brewed Coffee, Every Day
+                        </h3>
+                        <div class="button">
+                        <div class="form-group">
+                            <input type="email" class="form-control" id="emailInput" placeholder="Email">
+                        </div>
+                        <div>
+                            <button class="btn btn-default" type="button">
+                                <img src="img/credit-card.png" alt="" class="btn-img">
+                                Become a member
+                            </button>
+                        </div>   
+                        <div class="scroll">
+                            <div>
+                                <h6>Scroll Down<h6>
+                           <p> To Discover More</p>
+                            </div>
+                           <div>
+                            <img src="img/scroll-down 1.png" alt="" class="scroll-img">
+                           </div>
+                        </div>
+                    </div>
+
+
+
+                    </div>
+                    
+                </div>
+            </div>      
+
+                      <!-- <div class="container">
                           <h2>Serving You Ethically Sourced, Freshly Brewed Coffee, Every Day</h2>
+                            
+                            <a href="#" class="carousel-control-prev" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+
+                            <a href="#" class="carousel-control-next" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
+<a href="#" class="carousel-control-prev" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+
+                            <a href="#" class="carousel-control-next" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                             <div class="button">
                                 <div class="form-group">
                                     <input type="email" class="form-control" id="emailInput" placeholder="Email">
@@ -28,7 +93,7 @@
                             <img src="img/scroll-down 1.png" alt="" class="scroll-img">
                            </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
                </div>
         </div>
