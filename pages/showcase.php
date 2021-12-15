915ea97e561153b1a78d1b9c7c2816689f78d4e8
mamstha <86461385+mamstha@users.noreply.github.com>
2021-12-15 12:36:30 +0545

    metro-coffee

--- pages/showcase.php
+++ pages/showcase.php
@@ -57,51 +57,7 @@
                 </div>
             </div>      
 
-                      <!-- <div class="container">
-                          <h2>Serving You Ethically Sourced, Freshly Brewed Coffee, Every Day</h2>
-                            
-                            <a href="#" class="carousel-control-prev" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-
-                            <a href="#" class="carousel-control-next" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-<a href="#" class="carousel-control-prev" role="button" data-slide="prev">
-                                <span class="carousel-control-prev-icon"></span>
-                                <span class="sr-only">Previous</span>
-                            </a>
-
-                            <a href="#" class="carousel-control-next" role="button" data-slide="next">
-                                <span class="carousel-control-next-icon"></span>
-                                <span class="sr-only">Next</span>
-                            </a>
-                            <div class="button">
-                                <div class="form-group">
-                                    <input type="email" class="form-control" id="emailInput" placeholder="Email">
-                                </div>
-                                 <div>
-                                    <button class="btn btn-default" type="button">
-                                       <img src="img/credit-card.png" alt="" class="btn-img">
-                                       Become a member
-                                    </button>
-                                 </div>   
-
-                            </div>
-
-                        </div>
-                        <div class="scroll">
-                            <div>
-                                <h6>Scroll Down<h6>
-                           <p> To Discover More</p>
-                            </div>
-                           <div>
-                            <img src="img/scroll-down 1.png" alt="" class="scroll-img">
-                           </div>
-                        </div>
-                    </div> -->
+                      
                 </div>
                </div>
         </div>
